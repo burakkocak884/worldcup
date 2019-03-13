99307becb9d4644a3e02b2b04ebcbea41d6b7a7b
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-13 13:04:23 -0400

    first test run

--- lib/worldcup/cli.rb
+++ lib/worldcup/cli.rb
@@ -1,9 +1,43 @@
+require 'pry'
 class CLI
 	
-
+#binding.pry
 def run
-	puts "made a new cli instance"
-	Scraper.get_initial_data
+	
+	input = gets.strip
+	if input == "exit"
+		puts "Good Bye..."
+	elsif input == "teams"
+		Scraper.teams_data
+ #binding.pry
+
+			
+	end
+	#Scraper.get_initial_data
 end
 
+def list_of_commands
+    puts "Enter 'teams' to view list of the teams in WorldCup 2018"
+	sleep(0.9)
+	puts "Enter 'groups' to view list of the teams each group"
+	sleep(0.8)
+	puts "Enter 'quarter' to view list of the teams that made to Quarter Final in WorldCup 2018"
+	sleep(0.7)
+	puts "Enter 'semi' to view list of the teams that made to Semi Final in WorldCup 2018"
+	sleep(0.6)
+	puts "Enter 'final' to view list of the teams that made to Final in WorldCup 2018"
+	sleep(0.5)
+	puts "Enter 'winner' to view the team that won the final match"
+	sleep(0.4)
+	puts "Enter 'teams' to view list of the teams in WorldCup 2018"
+	sleep(0.3)
+	puts "Enter 'teams' to view list of the teams in WorldCup 2018"
+	sleep(0.2)
+	puts "Enter 'teams' to view list of the teams in WorldCup 2018"
+
+
+	end
+
 end
+
+# CLI.list_of_commands
--- lib/worldcup/scraper.rb
+++ lib/worldcup/scraper.rb
@@ -1,10 +1,43 @@
+require 'open-uri'
 require 'pry'
+require 'nokogiri'
 class Scraper
+	require_relative './team.rb'
 
 
-	def get_web_page
-		html = Nokogiri::HTML(open("https://www.nationalgeographic.com/people-and-culture/top-teams-countries-world-cup-graphic-interactive-culture/#secondpage"))
-		binding.pry
+		def self.get_web_page
+			
+			#binding.pry
+			html = Nokogiri::HTML(open("https://www.nationalgeographic.com/people-and-culture/top-teams-countries-world-cup-graphic-interactive-culture/#secondpage"))
+			#binding.pry
+			#puts "hello"
 
-	end
+		end
+
+
+		def self.teams_data
+			list_of_teams = []
+			#binding.pry
+			team_names = get_web_page.css(".ai2html")
+			team_names.each do |team_name|
+				#binding.pry
+				 list_of_teams << team_name.css("#g-ai1-3 p").text.upcase.strip
+				#binding.pry
+			end
+			list_of_teams.sort
+			#binding.pry
+		end
+
+		def self.champions
+
+
+
+		end
+
+
+
+
+
 end
+Scraper.get_web_page
+Scraper.teams_data
